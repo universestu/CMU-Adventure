6d7f7ca47e9ef21704d26ce6aee23ff443045fbd
ood9842 <ood9842@hotmail.com>
2016-10-24 12:53:47 +0700

    system game

---
+++ src/com/company/Sprite/Forest/Forest.java
@@ -0,0 +1,36 @@
+package com.company.Sprite.Forest;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Created by Chetsada Chaiprasop on 10/24/2016.
+ */
+public class Forest extends JPanel {
+    private ArrayList<BufferedImage> imgArr = new ArrayList<>();
+    private ImageLoader path = new ImageLoader();
+
+    public Forest() throws IOException {
+        for(int i=0;i<7;i++)
+        {
+            imgArr.add(path.loadImage("forest"+(i+1)+".png"));
+        }
+    }
+
+    public void draw(int x,int y,int index, Graphics g)
+    {
+        g.drawImage(imgArr.get(index),x,y,null);
+    }
+
+    public void tree01(int x,int y,Graphics g)
+    {
+        draw(x+30,y-29,1,g);
+        draw(x+0,y-29,1,g);
+        draw(x,y,2,g);
+        draw(x+60,y+0,2,g);
+        draw(x+30,y+0,2,g);
+    }
+}
